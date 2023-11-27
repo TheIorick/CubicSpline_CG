45a32f791e897fc7e769493d5a47085e35a620a8
The_Iorick <kotolevskij.vlad@mail.ru>
2023-11-28 00:51:30 +0300

    MISSION COMPLETE

--- src/main/java/com/cgvsu/protocurvefxapp/CubicSplineCurveController.java
+++ src/main/java/com/cgvsu/protocurvefxapp/CubicSplineCurveController.java
@@ -9,6 +9,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class CubicSplineCurveController {
@@ -19,7 +20,10 @@ public class CubicSplineCurveController {
     private Canvas canvas;
 
     ArrayList<Point2D> points = new ArrayList<Point2D>();
-
+    SplineRealization splineForX = new SplineRealization();
+    SplineRealization splineForY = new SplineRealization();
+    SplineRealization spline = new SplineRealization();
+    final int POINT_RADIUS = 3;
     @FXML
     private void initialize() {
         anchorPane.prefWidthProperty().addListener((ov, oldValue, newValue) -> canvas.setWidth(newValue.doubleValue()));
@@ -34,7 +38,7 @@ public class CubicSplineCurveController {
     private void handlePrimaryClick(GraphicsContext graphicsContext, MouseEvent event) {
         final Point2D clickPoint = new Point2D(event.getX(), event.getY());
 
-        final int POINT_RADIUS = 3;
+
         graphicsContext.fillOval(
                 clickPoint.getX() - POINT_RADIUS, clickPoint.getY() - POINT_RADIUS,
                 2 * POINT_RADIUS, 2 * POINT_RADIUS);
@@ -42,15 +46,57 @@ public class CubicSplineCurveController {
         if (points.size() == 1) {
             final Point2D lastPoint = points.get(0);
             graphicsContext.strokeLine(lastPoint.getX(), lastPoint.getY(), clickPoint.getX(), clickPoint.getY());
-        }if (points.size() > 1){
-            graphicsContext.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-            for (Point2D p : points){
-                graphicsContext.fillOval(
-                        clickPoint.getX() - POINT_RADIUS, clickPoint.getY() - POINT_RADIUS,
-                        2 * POINT_RADIUS, 2 * POINT_RADIUS);
-            }
-
         }
         points.add(clickPoint);
+        double ti[] = new double[points.size()];
+        double xi[] = new double[points.size()];
+        double yi[] = new double[points.size()];
+
+        if (points.size() > 2){
+            ti[0] = 0;
+            xi[0] = points.get(0).getX();
+            yi[0] = points.get(0).getY();
+            graphicsContext.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+            for (int i = 1; i < points.size(); i++){
+                graphicsContext.fillOval(
+                        points.get(i).getX() - POINT_RADIUS, points.get(i).getY() - POINT_RADIUS,
+                        2 * POINT_RADIUS, 2 * POINT_RADIUS);
+                ti[i] = calculateTi(points.get(i-1).getX(), points.get(i).getX(), points.get(i-1).getY(), points.get(i).getY()) + ti[i - 1];
+                xi[i] = points.get(i).getX();
+                yi[i] = points.get(i).getY();
+            }
+//            ti = calculateParams(xi, yi);
+            splineForX.searchCoeffSplines(ti, xi, xi.length);
+            splineForY.searchCoeffSplines(ti, yi, yi.length);
+            spline.searchCoeffSplines(xi, yi, points.size());
+            double interpolationX1, interpolationY1;
+            for(int i = 0; i < points.size()-2; i++){
+                for (double t = ti[i]; t < ti[i + 1]-0.1; t += 0.1){
+                    interpolationX1 = splineForX.realisationSpline(t, i);
+                    interpolationY1 = splineForY.realisationSpline(t, i);
+                    graphicsContext.fillOval(interpolationX1, interpolationY1, 2 * POINT_RADIUS, 2 * POINT_RADIUS);
+                }
+            }
+        }
+
     }
+
+    private double calculateTi(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+//    private double[] calculateParams(double[] x, double[] y) {
+//        double[] dx = Arrays.copyOfRange(x, 1, x.length);
+//        double[] dy = Arrays.copyOfRange(y, 1, y.length);
+//        double[] ds = new double[dx.length];
+//        for (int i = 0; i < dx.length; i++) {
+//            ds[i] = Math.sqrt(Math.pow(dx[i], 2) + Math.pow(dy[i], 2));
+//        }
+//        double[] s = new double[x.length];
+//        s[0] = 0.0;
+//        for (int i = 1; i < x.length; i++) {
+//            s[i] = s[i - 1] + ds[i - 1];
+//        }
+//        return s;
+//    }
 }
--- src/main/java/com/cgvsu/protocurvefxapp/SplineRealization.java
+++ src/main/java/com/cgvsu/protocurvefxapp/SplineRealization.java
@@ -5,56 +5,86 @@ import java.util.List;
 
 public class SplineRealization {
     List<CubicSpline> splines = new ArrayList<>();
-    public void searchCoeffSplines(double[] ti, double[] fi, int n){
+
+    public void searchCoeffSplines(double[] ti, double[] fi, int n) {
         splines.clear();
-        for (int i = 0; i < n - 1; i++){
+        for (int i = 0; i < n; i++) {
             CubicSpline spline = new CubicSpline(ti[i], fi[i]);
             splines.add(spline);
         }
         splines.get(0).setC(0.0);
-        splines.get(n).setC(0.0);
+        splines.get(n - 2).setC(0.0);
         searchCi(ti, fi, n);
         double hi, ci1, ci;
-        for (int i = 0; i < n - 1; i++){
+        for (int i = 0; i < n - 1; i++) {
             hi = ti[i + 1] - ti[i];
             ci1 = splines.get(i + 1).getC();
             ci = splines.get(i).getC();
-            splines.get(i).setB(((fi[i+1] - fi[i])/hi) - (hi/3)*(ci1 + 2*ci));
-            splines.get(i).setD((ci1 - ci)/ 3*hi);
+            splines.get(i).setB(((fi[i + 1] - fi[i]) / hi) - (hi / 3) * (ci1 + 2 * ci));
+            splines.get(i).setD((ci1 - ci) / (3 * hi));
         }
-        splines.get(n).setB(0.0);
-        splines.get(n).setD(0.0);
+        splines.get(n - 2).setB(0.0);
+        splines.get(n - 2).setD(0.0);
     }
 
-    private void searchCi(double[] ti, double[] fi, int n){
-        double[] Vi = new double[n];
-        double[] Ui = new double[n];
+    public void searchCi(double[] ti, double[] fi, int n) {
+        double[] Vi = new double[n+1];
+        double[] Ui = new double[n+1];
         Vi[0] = 0.0;
         Ui[0] = 0.0;
         double A, B, C, D, hi, hi1, del;
 
-        for (int i = 1; i < n - 1; i++){
-            //todo: проверить потом, что тут не так было у Нелли
+        for (int i = 1; i < n; i++) {
             hi = ti[i] - ti[i - 1];
-            hi1 = ti[i + 1] - ti[i];
-            A = 2*(hi + hi1);
-            B = hi1;
             C = hi;
-            D = 3*(((fi[i + 1] - fi[i]) / hi1) - ((fi[i] - fi[i - 1])/ hi));
-            del = C * Vi[i - 1] + A;
-            Vi[i] = - (B / del);
-            Ui[i] =  (D - C*Ui[i-1]) / del;
+            if (i + 1 == ti.length) {
+                hi1 = 0;
+                D = 3.0 *  fi[i] / hi;
+            } else {
+                hi1 = ti[i + 1] - ti[i];
+                D = 3.0 * ((fi[i + 1] - fi[i]) / hi1 - (fi[i] - fi[i - 1]) / hi);
+            }
+            A = 2 * (hi + hi1);
+            B = hi1;
+            del =  C * Vi[i - 1] + A;
+            Vi[i] = -(B / del);
+            Ui[i] = (D - C * Ui[i - 1]) / del;
         }
-
-        //todo: тут тоже чек нужен
-        for (int i = n - 1; i > 0; i--){
+        splines.get(n - 1).setC(Ui[n - 1]);
+        for (int i = n - 2; i > 0; i--) {
             C = splines.get(i + 1).getC();
-            splines.get(i).setC(Vi[i]*C + Ui[i]);
+            splines.get(i).setC(Vi[i] * C + Ui[i]);
         }
     }
 
-    public double realisationSpline(double ti, int i){
+    public double realisationSpline(double ti, int i) {
         CubicSpline s = splines.get(i);
-        return s.getA() + s.getB()*(ti - s.getX()) + s.getC()*Math.pow(ti - s.getX(), 2) + s.getD()*Math.pow(ti - s.getX(), 3);
+        return s.getA() + s.getB() * (ti - s.getX()) + s.getC() * Math.pow(ti - s.getX(), 2) + s.getD() * Math.pow(ti - s.getX(), 3);
+    }
+
+    public Double interpolate(double x) {
+        if (splines.isEmpty()) {
+            return null;
+        }
+
+        int n = splines.size();
+        CubicSpline s;
+
+        if (x <= splines.get(0).getX() || x >= splines.get(n - 1).getX()) {
+            return Double.NaN;
+        }
+        int i = 0;
+        int j = n - 1;
+        while (i < j) {
+            int k = (i + j) / 2;
+            if (x <= splines.get(k).getX()) {
+                j = k;
+            } else {
+                i = k + 1;
+            }
+        }
+        s = splines.get(i - 1);
+        double dx = (x - s.getX());
+        return s.getA() + s.getB() * dx + s.getC() * Math.pow(dx, 2) + s.getD() * Math.pow(dx, 3);
     }
 }
